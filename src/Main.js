3a96f3dac2c734bc6beb1462f90d5b4542e4c61e
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-04-22 12:53:46 -0300

    Criação de estrutura básica

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+
+import News from './screens/News/News'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <View>
+        <News />
+      </View>
+    )
+  }
+}
